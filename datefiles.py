1b691c08910ffc1edde267424552c135a0c3afe6
Your Name <you@example.com>
2024-03-25 19:16:02 +0100

    minor

--- datefiles.py
+++ datefiles.py
@@ -7,12 +7,23 @@ from datetime import datetime
 from pathlib import Path
 
 
+ignoreExtensions = [".lnk"]
+
+
 def addDatePrefix(folders):
     for thefolderStr in folders:
         print("\n===== " + thefolderStr + " #####")
         thefolder = Path(thefolderStr)
         for c in thefolder.iterdir():
             if c.is_file():
+                ignoreThis = False
+                for ie in ignoreExtensions:
+                    if c.name.lower().endswith(ie):
+                        ignoreThis = True
+                        break
+                if ignoreThis:
+                    continue
+
                 theprefix = datetime.fromtimestamp(c.stat().st_mtime).strftime("%Y%m%d") + "_"
                 if not c.name.startswith(theprefix):
                     newFile = thefolder / (theprefix + c.name)
